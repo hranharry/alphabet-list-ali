57a3cc632f1954ecdf8a573e8f32c18d5a0473cf
张浩然 <hranharry@sina.com>
2019-07-14 17:26:57 +0800

    修复滚动错误  滑动冲突

--- src/alphabet-list/alphabet-list.js
+++ src/alphabet-list/alphabet-list.js
@@ -2,8 +2,10 @@ Component({
   mixins: [],
   data:{
     alpha: 0 ,
+    windowHeight: 0,
     scrollHeight: 0,
     offsetTop: 0,
+    scrollViewTop: 0,
     apTabOffsetTop: 0
   },
   props: {
@@ -18,20 +20,25 @@ Component({
     try {
       my.createSelectorQuery()
         .select('#Top').boundingClientRect()
-        .select('#alphabet-tab').boundingClientRect()
+        .select('.alphabet-in-scroll').boundingClientRect()
+        .select('.alphabet-in-selector').boundingClientRect()
         .exec(res => {
           let sysInfo = my.getSystemInfoSync()
-          if (res && res.length === 2 && this.props.list && this.props.list.length > 0) {
-            let offsetTop = res[0].height;
-            let apTabOffsetTop = res[1].top
+          if (res && res.length === 3 && this.props.list && this.props.list.length > 0) {
+            let headerOffsetTop = res[0].height;
+            let scrollViewTop = res[1].top
+            let apTabOffsetTop = res[2].top
+            console.log(res)
             this.setData({
-              scrollHeight: sysInfo.windowHeight - offsetTop  + 'px',
+              windowHeight: sysInfo.windowHeight,
+              scrollHeight: sysInfo.windowHeight - headerOffsetTop  + 'px',
+              scrollViewTop: scrollViewTop,
               apTabOffsetTop: apTabOffsetTop
             })
           }
         })
     } catch (e) {
-      
+      console.log(e)
     }
   },
   didUpdate() {},
@@ -42,16 +49,27 @@ Component({
       this.setData({alpha: ap});
     },
     handlerMove(e) {
-      let {list} = this.props;
+      let touchIndex = e.target.targetDataset.index
+      let touchOffsetTop = e.target.offsetTop + (this.data.windowHeight * 0.05)
       let moveY = e.touches[0].clientY;
-      let rY = moveY - this.data.apTabOffsetTop;
-      if(rY >= 0) {
-        let index = Math.ceil((rY - this.props.apHeight)/ this.props.apHeight);
-        if(0 <= index < list.length) {
-          let nonwAp = list[index];
-          nonwAp && this.setData({alpha: nonwAp.key});
-        } 
+      if (touchIndex > -1) {
+        let {list} = this.props;
+        let rY = moveY - touchOffsetTop;
+          let index = Math.ceil((rY - this.props.apHeight) / this.props.apHeight) + touchIndex;
+          if (index < 0) {
+            this.setData({
+              alpha: 0
+            })
+          } else if(index < list.length) {
+            let nonwAp = list[index];
+            nonwAp && this.setData({alpha: nonwAp.key});
+          } else {
+            this.setData({
+              alpha: list.length
+            })
+          }
       }
+      
     },
     onClickItem(e) {
       this.props.onChoose(e.currentTarget.dataset.item)
